954df632689f02682939521d1bd1f4a2c42d279b
M. Abdullah Khan <angkonkhan206@gmail.com>
2025-10-28 12:30:03 +0600

    Task7.cpp

---
+++ Task7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string firstName, lastName;
+
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    string fullName = firstName + " " + lastName;
+
+    cout << "Full Name = " << fullName << endl;
+
+    return 0;
+}
